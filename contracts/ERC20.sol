713eb99fdd78e50e89d5f50667c71512945872b5
Joy A <joy.gold13@gmail.com>
2025-08-09 14:26:41 +0100

    first commit

---
+++ contracts/ERC20.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
+
+contract ERC20 is IERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 private _totalSupply;
+    address public owner;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    constructor() {
+        name = "Gold Token";
+        symbol = "GTKoin";
+        owner = msg.sender;
+    }
+
+    function totalSupply() external view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 amount) external override returns (bool) {
+        require(to != address(0), "Invalid address");
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
+
+        _balances[msg.sender] -= amount;
+        _balances[to] += amount;
+        return true;
+    }
+
+    function allowance(address _owner, address spender) external view override returns (uint256) {
+        return _allowances[_owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) external override returns (bool) {
+        require(spender != address(0), "Invalid address");
+        _allowances[msg.sender][spender] = amount;
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external override returns (bool) {
+        require(to != address(0), "Invalid address");
+        require(_balances[from] >= amount, "Insufficient balance");
+        require(_allowances[from][msg.sender] >= amount, "Allowance exceeded");
+
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        _allowances[from][msg.sender] -= amount;
+        return true;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _totalSupply += amount;
+        _balances[to] += amount;
+    }
+
+    function burn(uint256 amount) external {
+        require(_balances[msg.sender] >= amount, "Not enough tokens to burn");
+        _balances[msg.sender] -= amount;
+        _totalSupply -= amount;
+    }
+}
